c02dc1206a5ad2fbeccf304ea68677338b6928cb
Freek Van der Herten <freek@spatie.be>
2026-02-23 23:54:16 +0100

    Make setUp() protected for Pest 3 compatibility

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,7 +10,7 @@ use Spatie\PrefixedIds\Tests\Database\Migrations\CreateTestModelsTable;
 
 class TestCase extends Orchestra
 {
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
