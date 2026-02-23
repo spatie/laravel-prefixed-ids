4236ca871d49b353fb86b040034e1a12c6c9a26c
Freek Van der Herten <freek@spatie.be>
2026-02-24 00:04:42 +0100

    Remove spatie/ignition dependency (not compatible with L13 yet)

--- src/Exceptions/NoPrefixConfiguredForModel.php
+++ src/Exceptions/NoPrefixConfiguredForModel.php
@@ -4,25 +4,13 @@ namespace Spatie\PrefixedIds\Exceptions;
 
 use Exception;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Ignition\Contracts\BaseSolution;
-use Spatie\Ignition\Contracts\ProvidesSolution;
-use Spatie\Ignition\Contracts\Solution;
 
-class NoPrefixConfiguredForModel extends Exception implements ProvidesSolution
+class NoPrefixConfiguredForModel extends Exception
 {
     public static function make(Model $model)
     {
         $modelClass = $model::class;
 
         return new static("Could not generate a prefixed id for model `{$modelClass}`");
-    }
-
-    public function getSolution(): Solution
-    {
-        return BaseSolution::create('Register the prefix of the model')
-            ->setSolutionDescription("You should register your model using `Spatie\PrefixedIds\PrefixedIds::registerModel()` in a service provider")
-            ->setDocumentationLinks([
-                'Documentation' => 'https://github.com/spatie/laravel-prefixed-ids#registering-models-with-prefixed-ids',
-            ]);
     }
 }
--- src/Exceptions/NoPrefixedModelFound.php
+++ src/Exceptions/NoPrefixedModelFound.php
@@ -3,23 +3,11 @@
 namespace Spatie\PrefixedIds\Exceptions;
 
 use Exception;
-use Spatie\Ignition\Contracts\BaseSolution;
-use Spatie\Ignition\Contracts\ProvidesSolution;
-use Spatie\Ignition\Contracts\Solution;
 
-class NoPrefixedModelFound extends Exception implements ProvidesSolution
+class NoPrefixedModelFound extends Exception
 {
     public static function make(string $prefixedId)
     {
         return new static("Could not find a prefixed model `{$prefixedId}`");
-    }
-
-    public function getSolution(): Solution
-    {
-        return BaseSolution::create('Make sure the prefix of the model exists')
-            ->setSolutionDescription("You should make sure your model has a valid `prefixed_id`")
-            ->setDocumentationLinks([
-                'Documentation' => 'https://github.com/spatie/laravel-prefixed-ids#usage',
-            ]);
     }
 }
